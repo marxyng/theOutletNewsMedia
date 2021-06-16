599adaf842a34d4499073699f64b263cf1ed11c0
marxyng <38600509+marxyng@users.noreply.github.com>
2021-06-15 21:40:18 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const likeable = document.querySelector('.likable') ; 
+
+
+likeable.addEventListener('click', likely)
+
+function likely  () {
+   
+   
+   countUp.innerHTML++;
+console.log(countUp);
+}
+ 
+
+    
+
+    
+    
+
+
+
+
+    
+
